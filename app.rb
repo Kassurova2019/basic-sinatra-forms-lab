5986b40125c4b673e824f98b9f611aff32a9b059
Luba Kassurova <kassurova@icloud.com>
2023-03-09 23:27:09 -0500

    commit tree 3bd9fd0ecc403993454e4063c4cd9f1e178b83e3

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
+require "pry"
 require 'sinatra/base'
 
 class App < Sinatra::Base
+ get '/newteam' do
+    erb :newteam
+ end
 
-
+ post '/team' do
+   #binding.pry
+   @team_name = params[:name]
+   @coach = params[:coach]
+   @pg = params[:pg]
+   @sg = params[:sg]
+   @sf = params[:sf]
+   @pf = params[:pf]
+   @c = params[:c]
+    erb :team
+ end
+ post '/newteam' do
+    "Hello World"
+  end
 end
